5ce705f1f00b84342a7fe4dbe6155cbc3f118f7c
Rongxin <rongxin@u.nus.edu>
2025-07-28 17:57:38 +0800

    fmt: black

--- pkgman/__init__.py
+++ pkgman/__init__.py
@@ -1,9 +1,10 @@
-'''
+"""
 Date: 2024-11-27 18:39:30
 LastEditors: Rongxin rongxin@u.nus.edu
 LastEditTime: 2025-07-28 17:55:23
 FilePath: /pkgman/pkgman/__init__.py
-'''
+"""
+
 from importlib import import_module
 from subprocess import check_call
 from sys import _getframe, executable
@@ -12,9 +13,7 @@ from sys import _getframe, executable
 def include(*args):
     if args is None or args == "" or len(args) == 0:
         print("[pkgman] no packages are given, skipped.")
-    elif (
-        (isinstance(args, list) or isinstance(args, tuple))
-            and len(args) == 1):
+    elif (isinstance(args, list) or isinstance(args, tuple)) and len(args) == 1:
         package_name = args[0]
         try:
             module = import_module(package_name)
@@ -24,9 +23,7 @@ def include(*args):
             return True
         except ImportError:
             print(f"[pkgman] Installing package(s) [{package_name}]...")
-            check_call(
-                [executable, "-m", "pip", "install", "-q", package_name]
-            )
+            check_call([executable, "-m", "pip", "install", "-q", package_name])
             try:
                 module = import_module(package_name)
                 caller_globals = _getframe(1).f_globals
@@ -37,9 +34,8 @@ def include(*args):
                 return False
         except Exception:
             return False
-    elif (
-            (isinstance(args, list) and len(args) > 1)
-            or (isinstance(args, tuple) and len(args) > 1)
+    elif (isinstance(args, list) and len(args) > 1) or (
+        isinstance(args, tuple) and len(args) > 1
     ):
         result_list = []
         for package_name in args:
@@ -50,9 +46,7 @@ def include(*args):
                 result_list.append(True)
             except ImportError:
                 print(f"[pkgman] Installing and importing {args}...")
-                check_call(
-                    [executable, "-m", "pip", "install", "-q", package_name]
-                )
+                check_call([executable, "-m", "pip", "install", "-q", package_name])
                 try:
                     module = import_module(package_name)
                     caller_globals = _getframe(1).f_globals
@@ -61,9 +55,7 @@ def include(*args):
                 except ImportError:
                     result_list.append(False)
             except Exception as e:
-                print(
-                    f"[pkgman] Error in installing {package_name}:\n{e}"
-                )
+                print(f"[pkgman] Error in installing {package_name}:\n{e}")
                 result_list.append(False)
         # If all results are True
         if result_list:
